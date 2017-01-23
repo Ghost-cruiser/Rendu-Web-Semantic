504ddbf65bff2913b746606bdb42163269f8741d
Marie Florian <florian.marie@etu.unice.fr>
2017-01-23 11:44:12 +0100

    Archivage : stylisation de l'app

---
+++ TD 2 - NodeJS/routes/admin/profile.js
@@ -0,0 +1,46 @@
+﻿"use strict";
+
+
+module.exports = function (app, config, router, pagehelper) {
+
+    var models = app.get("models");
+
+    router
+
+        .route('/admin/profile')
+
+        // GET /user/profile 
+        // Get the user's profile
+        .get(function (req, res) {
+            // USER
+            models.User.findOne({ where: { id: req.session.passport.user.id } }).then(function (result) {
+                pagehelper
+                    .render(res, 'admin', 'user', result.get(), 'Profile');
+
+            }).catch(function (error) {
+                pagehelper
+                    .sendError(res, error);
+            });
+
+        })
+
+        // POST /user/profile 
+        // Updates the profile 
+        .post(function (req, res) {
+            var user = req.body;
+
+            // UPDATE 
+            models.User.update(req.body, { where: { id: req.session.passport.user.id } }).then(function () {
+                pagehelper
+                    .redirect(res, 'admin', 'index', '', {
+                        message: 'Profile mis à jour!'
+                    });
+
+            }).catch(function (error) {
+                pagehelper
+                    .sendError(res, error);
+            });
+
+        });
+
+}
